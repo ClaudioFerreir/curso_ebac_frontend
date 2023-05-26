04956feef342ae7a8812c67eaf6e50e88e937eca
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-05-26 18:38:38 -0300

    js file updated

--- js/mainFormJS.js
+++ js/mainFormJS.js
@@ -11,16 +11,19 @@ form.addEventListener('submit', function(e) {
   const successMessage = `<i>AMAZING! YOU'RE RIGHT!!! The number <b>${numberB.value}</b> is greater than <b>${numberA.value}</b></i>`;
   const errorMessage = `<i>OPS! TRY AGAIN!!! The number <b>${numberB.value}</b> is <b>NOT</b> greater than <b>${numberA.value}</b></i>`;
 
-  console.log(numberA.value <= numberB.value);
+  console.log(numberA.value < numberB.value);
   
   if (numberA.value < numberB.value) {
+    containerResult.classList.remove('error-message');
     containerResult.innerHTML = successMessage;
+    containerResult.style.display = 'block';
     containerResult.classList.add('success-message');  
 
     numberA.value = '';
     numberB.value = '';
   } else {
     numberB.style.border = '1px solid red';
+    containerResult.classList.remove('success-message');
     containerResult.innerHTML = errorMessage;
     containerResult.style.display = 'block';
     containerResult.classList.add('error-message');
