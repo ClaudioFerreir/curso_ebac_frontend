d85973a6b4c7db44716d01327967375d917e5d2d
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-06-02 14:15:03 -0300

    feat: add task

--- SimpleToDoList/main.js
+++ SimpleToDoList/main.js
@@ -11,8 +11,20 @@ $(document).ready(function() {
 
   $('form').on('submit', function(e) {
     e.preventDefault();
+    AddTask();
+  })
+
+  function AddTask() {
     const newTask = $('#new-task').val();
     const newPriority = $('#new-priority').val();
-    console.log(newTask, newPriority);
-  })
+    const addTask = $(`<li style="display: none" class=${newPriority}></li>`);
+    $(addTask).append(`<p class="task">${newTask}</p>`);
+    $(addTask).append(`<p class="priority">${newPriority}</p>`);
+    $('ul').append(addTask);
+    $(addTask).fadeIn(1000);
+
+    $('#new-task').val('');
+    $('#new-priority').val('');
+  }
 });
+
